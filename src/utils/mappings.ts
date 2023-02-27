bd2d8788595d5dee04bb38abf5034574990131f7
Sebastian <olastor@users.noreply.github.com>
2023-02-27 21:40:27 +0100

    fix: add missing file

---
+++ src/utils/mappings.ts
@@ -0,0 +1,44 @@
+import { FIELD_CLASSES } from '../constants'
+import { Mappings, MappingType } from '../interfaces'
+
+export const validateMappings = (
+  nestedMappings: any
+): Mappings => {
+  if (typeof nestedMappings !== 'object') {
+    throw new Error('Mappings must be an object!')
+  }
+
+  let mappings: Mappings = {}
+
+  const flatten = (
+    obj: { [key: string]: any },
+    currentPath: string[] = []
+  ): void => {
+    for (const [key, value] of Object.entries(obj)) {
+      if (typeof value === 'string') {
+        const path = [...currentPath, key].join('.')
+
+        if (!FIELD_CLASSES[value]) {
+          throw new Error(`Invalid mapping type '${value}' for field '${path}'.`)
+        }
+
+        mappings[path] = value as MappingType
+      }
+
+      if (Array.isArray(value)) {
+        throw new Error('Mappings must not contain arrays!')
+      }
+
+      if (typeof value === 'object') {
+        return flatten(value, [...currentPath, key])
+      }
+
+      const path = [...currentPath, key].join('.')
+      throw new Error(`Invalid value of type '${typeof value}' found at '${path}'.`)
+    }
+  }
+
+  flatten(nestedMappings)
+
+  return mappings
+}
