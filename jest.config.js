ca448cc292daf4bfc05351ce07dc8a6d4b69fe3e
Sebastian <>
2023-03-05 20:36:30 +0100

    chore: enable all tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
-  // TODO: run all tests
-  testRegex: '/test/high.*\\.(test|spec)?\\.(ts|tsx)$',
+  testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testTimeout: 300000,
   maxWorkers: '20%',
