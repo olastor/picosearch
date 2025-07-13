81e4b3504b8338bb0739fc36b502c3283cd04a32
sebastian <>
2025-07-13 20:25:48 +0200

    chore: remove unused file

--- packages/picosearch/browser-entry.js
+++
@@ -1,5 +0,0 @@
-import { Picosearch } from './src/index';
-import * as englishOptions from '@picosearch/language-english';
-
-window.Picosearch = Picosearch;
-window.picosearchLanguageEnglish = englishOptions; 
