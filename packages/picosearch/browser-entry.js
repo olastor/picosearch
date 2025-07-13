ed66e6c69353a3f5995f2a795ae8e0af3c571bfd
sebastian <>
2025-07-13 20:24:19 +0200

    chore: add esm builds

---
+++ packages/picosearch/browser-entry.js
@@ -0,0 +1,5 @@
+import { Picosearch } from './src/index';
+import * as englishOptions from '@picosearch/language-english';
+
+window.Picosearch = Picosearch;
+window.picosearchLanguageEnglish = englishOptions; 
